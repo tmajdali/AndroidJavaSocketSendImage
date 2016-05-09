6e01d16233fbf5594c40f5d0a117034edf6efce4
tmajdali <t.majdaly@gmail.com>
2016-05-08 21:49:06 -0500

    c02

--- MyGalleryTest/app/src/main/java/com/example/hsport/mygallerytest/MainActivity.java
+++ MyGalleryTest/app/src/main/java/com/example/hsport/mygallerytest/MainActivity.java
@@ -16,16 +16,43 @@ package com.example.hsport.mygallerytest;
         import android.widget.Toast;
         import java.io.*;
         import java.net.*;
-
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private static int RESULT_LOAD_IMG = 1;
     String imgDecodableString;
-
+    Button b1;
+    EditText ed01;
+    String imagePath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.button02);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String fname = ((EditText) findViewById(R.id.editText)).getText().toString();
+                ed01 = (EditText) findViewById(R.id.editText);
+
+                //Client myClient = new Client(fname + ";" + sname);
+                // myClient.execute();
+
+                //Log.d("FROMSERVER", Boolean.toString(myClient.flag));
+                if (fname.equals("")) {
+
+                    ed01.setHint("Please Enter Plate Number!");
+                } else {
+                    Client myClient = new Client(imagePath);
+                    myClient.execute();
+                }
+
+            }
+        });
     }
 
     public void loadImagefromGallery(View view) {
@@ -55,15 +82,16 @@ public class MainActivity extends Activity {
                 cursor.moveToFirst();
 
                 int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                String imagePath = cursor.getString(cursor.getColumnIndex(filePathColumn[0]));
+                 imagePath = cursor.getString(cursor.getColumnIndex(filePathColumn[0]));
                 imgDecodableString = cursor.getString(columnIndex);
                 cursor.close();
                 ImageView imgView = (ImageView) findViewById(R.id.imgView);
                 // Set the Image in ImageView after decoding the String
                 imgView.setImageBitmap(BitmapFactory
                         .decodeFile(imgDecodableString));
-                Client myClient = new Client(imagePath);
-                myClient.execute();
+
+
+
 
 
 
@@ -87,7 +115,7 @@ public class MainActivity extends Activity {
         @Override
         protected Void doInBackground(Void... params) {
 try{
-    String hostName = "IP here";
+    String hostName = "205.178.20.186";
     int portNumber = 3003;
 
 
@@ -102,9 +130,10 @@ try{
 
 
                 int bytesRead;
-                while ( (bytesRead = in.read(buffer )) != -1 ) {
+                while ( (bytesRead = in.read(buffer )) > -1 ) {
                     out.write( buffer, 0, bytesRead );
                 }
+                out.flush();
                 socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
